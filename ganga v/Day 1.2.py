a0e4630b1cdab87b16b375e19b4a9918adf77540
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-09 23:33:33 +0530

    Add files via upload

---
+++ ganga v/Day 1.2.py
@@ -0,0 +1,9 @@
+'''
+author:ganga
+version:1.1
+date:9-11-2024
+python program to find circumference of given circle
+'''
+radius = int(input("Enter radius of the circle:"))
+circumference=2*3.14*radius
+print("Circumference of the given circle is ",circumference)
