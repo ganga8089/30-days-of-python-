94daacbba24d19ec8cd69ba903e1e7ef2620f081
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-11-09 23:24:55 +0530

    Add files via upload

---
+++ joshwin/rectangle.py
@@ -0,0 +1,12 @@
+'''Joshwin James
+9-11-2024
+find area of rectangle'''
+
+ln=int(input("Enter the length of the rectangle"))
+br=int(input("Enter the breadth of he rectangle"))
+#finding the area
+ar=(ln*br)
+print("Area of rectangle=",ar)
+
+#perimeter
+print("perimeter=",2*(ln+br))
