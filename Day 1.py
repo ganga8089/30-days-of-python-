d62ab095fd7063612db9b30090121504899a2690
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-11-09 22:54:13 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,6 @@
+lenght=int(input("lenght of the rectangle:"))
+breadth=int(input("breadth of the rectangle:"))
+area=lenght*breadth
+perimeter=2*(lenght+breadth)
+print("Area of the rectangle is:",area)
+print("Perimeter of the rectangle is:",perimeter)
