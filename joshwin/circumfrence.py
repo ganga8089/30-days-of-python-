c20f8b90f108dd7eb71624181f489cc10ec373b6
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-11-09 23:21:43 +0530

    day 1

---
+++ joshwin/circumfrence.py
@@ -0,0 +1,8 @@
+'''Joshwin James
+9-11-2024
+find circumfrence of circle'''
+
+r=int(input("Enter the radius of the circle"))
+#finding the circumfrence
+cr=(2*(3.14)*r)
+print("Circumfrence of circle=",cr)
