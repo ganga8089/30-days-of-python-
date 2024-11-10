4566ca269f4f04566210971fb17d49ed48aea41a
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-10 21:56:06 +0530

    find text

---
+++ joswin/Day 2-2.py
@@ -0,0 +1,9 @@
+'''author:Joswin Shaju
+date:10/11/24
+to find a string'''
+
+str="Python is a widely used general-purpose, high level programming language. It was initially designed by Guido van Rossum in 1991 and developed by Python Software Foundation."
+if "1991" in str:
+    print("found text")
+else:
+    print("text not found")
