546888f7d49459ef57c03824a6c2ebfa6de1c635
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-10 22:16:30 +0530

    Add files via upload

---
+++ joswin/Day 2-1.py
@@ -0,0 +1,8 @@
+'''author:Joswin Shaju
+date:10/11/24'''
+import math
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+divd=num2/num1
+mult=divd*math.sqrt(num2)
+print("the number is:",mult)
