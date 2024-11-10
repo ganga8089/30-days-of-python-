95e41c5f7cec15dc42a70c20a82e91c8cfdba737
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-11-10 22:05:14 +0530

    Add files via upload

---
+++ joseph/Day 2 first.py
@@ -0,0 +1,8 @@
+import math
+
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+division=num1/num2
+square_root=math.sqrt(division)
+result=divis4ion*square_root
+print(f"The final result is:{result}")
