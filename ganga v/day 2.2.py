b9ca39c386b0f8cdb33a2a827bde931275c7b5a7
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-10 21:59:37 +0530

    Add files via upload

---
+++ ganga v/day 2.2.py
@@ -0,0 +1,8 @@
+'''
+name:ganga
+date:10-11-2024
+version 1.1
+python program to find the presence of a word in a given string
+'''
+string="Python is a widely used general-purpose, high level programming language. It was initially designed by Guido van Rossum in 1991 and developed by Python Software Foundation."
+print("1991" in string)
