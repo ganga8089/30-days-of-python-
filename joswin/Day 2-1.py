b2657ce9e19ae58ebee77c0042d089b5927627e9
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-10 22:10:24 +0530

    Delete joswin/Day 2-1.py

--- joswin/Day 2-1.py
+++
@@ -1,8 +0,0 @@
-'''author:Joswin Shaju
-date:10/11/24'''
-
-num1=int(input("enter the number:"))
-num2=int(input("enter the number:"))
-divd=num2/num1
-mult=divd*(num2**2)
-print("the number is:",mult)
