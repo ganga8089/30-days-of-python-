8fdb2be4cc7f3dba16a2c4a373f679017b984a86
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-10 21:55:35 +0530

    mathematical operations

---
+++ joswin/Day 2-1.py
@@ -0,0 +1,8 @@
+'''author:Joswin Shaju
+date:10/11/24'''
+
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+divd=num2/num1
+mult=divd*(num2**2)
+print("the number is:",mult)
