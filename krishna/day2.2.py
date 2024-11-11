34fdbe15444011924602c3191319637b7fffc33a
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-12 00:08:43 +0530

    Add files via upload

---
+++ krishna/day2.2.py
@@ -0,0 +1,5 @@
+'''
+python program to find the presence of a word in a given string
+'''
+string="python is a widely used general-purpose,high level programming language. it was initially designed by guido van rossum in 1991 and developed by python software foundation."
+print("1991" in string)
