edcac85acdd89ff5c398eac79670a3b8b3763a67
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-11 23:51:06 +0530

    Add files via upload

---
+++ ganga v/day3.1.py
@@ -0,0 +1,11 @@
+'''
+name:ganga
+date:11-11-2024
+version1.1
+python program to calculate the power of a number using the ** operator.
+'''
+number=int(input("Enter a number:"))
+power=int(input("Enter power to be raised:"))
+exponentiation=number**power
+print(f"Exponentiation of the given number {number} raised to {power} is {exponentiation} ")
+
