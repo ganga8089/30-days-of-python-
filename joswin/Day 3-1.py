f81025936bf26b9f497f9de6259a70703ba46fa4
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-11 23:33:20 +0530

    Delete joswin/Day 3-1.py

--- joswin/Day 3-1.py
+++
@@ -1,8 +0,0 @@
-'''author:Joswin Shaju
-date:11/11/24
-to calculate the power of a number'''
-
-num=int(input("enter the number:"))
-pow=int(input("enter the power:"))
-val=num**pow
-print(f"the power of {num} is:",val)
