52393842e631bc4870eaa650ac0e2d9a0ce74338
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-11 23:17:01 +0530

    pow with **

---
+++ joswin/Day 3-1.py
@@ -0,0 +1,8 @@
+'''author:Joswin Shaju
+date:10/11/24
+to calculate the power of a number'''
+
+num=int(input("enter the number:"))
+pow=int(input("enter the power:"))
+val=num**pow
+print(f"the power of {num} is:",val)
