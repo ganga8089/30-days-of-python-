203ae17345c5b4a04c0428f35eaf5f83e6214b73
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-12 00:03:40 +0530

    Add files via upload

---
+++ ganga v/day3.2.py
@@ -0,0 +1,13 @@
+'''
+name:ganga
+date:11-11-2024
+version1.1
+python program to performs basic operations (addition, subtraction, multiplication, and division) on two numbers input by the user.
+'''
+number_1=int(input("Enter a number:"))
+number_2=int(input("Enter second number:"))
+addition=number_1 + number_2
+substraction=number_2-number_1
+multiplication=number_1*number_2
+division=number_2/number_1
+print(f"Addition= {addition} \n Substraction= {substraction} \n Multiplication= {multiplication} \n Division= {division} ")
