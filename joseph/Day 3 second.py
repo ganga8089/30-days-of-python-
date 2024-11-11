d3090d7b74cbc88b1dba1f7fc9f3d321bc3d8e90
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-11-11 23:14:15 +0530

    Add files via upload

---
+++ joseph/Day 3 second.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+addition=num1+num2
+substraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print(num1,"+",num2,"=",addition)
+print(num1,"-",num2,"=",substraction)
+print(num1,"*",num2,"=",multiplication)
+print(num1,"/",num2,"=",division)
