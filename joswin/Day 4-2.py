ce59dc341bc8e18b10602b1df60feae14573ec5b
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-12 23:02:47 +0530

    Add files via upload

---
+++ joswin/Day 4-2.py
@@ -0,0 +1,7 @@
+'''author:Joswin Shaju
+date:12/11/24
+to take first and last colour'''
+
+colour_list=["red","green","white","black"]
+print("first colour - ",colour_list[0])
+print("last colour - ",colour_list[3])
