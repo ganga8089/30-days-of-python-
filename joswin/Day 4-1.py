fdf7815a56f46fa454972396852f0a12cc13dc8e
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-12 23:01:34 +0530

    reverse name

---
+++ joswin/Day 4-1.py
@@ -0,0 +1,8 @@
+'''author:Joswin Shaju
+date:12/11/24
+to reverse first and last name'''
+
+first_name=input("enter first name:")
+last_name=input("enter the last name:")
+full_name=first_name+last_name
+print("in reverse:",full_name[::-1])
