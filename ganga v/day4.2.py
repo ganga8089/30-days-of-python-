d103ce28a987c967280144478ddc83a2e1c63fd4
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-12 21:34:31 +0530

    Add files via upload

---
+++ ganga v/day4.2.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:12-11-2024
+version1.1
+ Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+'''
+color_list=["Red","Green","White","Black"]
+print(f"First colour= {color_list[0]} ,last colour= {color_list[3]}")
