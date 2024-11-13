6672914a2ae47e7173849877b980b42c3e731fba
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-13 23:42:56 +0530

    Add files via upload

---
+++ joswin/Day 5-1.py
@@ -0,0 +1,8 @@
+'''author:Joswin Shaju
+date:13/11/24
+area of a triangle'''
+
+base=int(input("enter base:"))
+height=int(input("enter height:"))
+area=(1/2)*(base*height)
+print("the area of a triangle is:",area)
