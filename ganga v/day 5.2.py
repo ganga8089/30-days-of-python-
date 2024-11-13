a9b6a4a27e3f4ef6113d0591eed4eae1128a1fa7
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-13 21:46:44 +0530

    Add files via upload

---
+++ ganga v/day 5.2.py
@@ -0,0 +1,10 @@
+'''
+name:ganga
+date:13-11-2024
+version1.1
+ Python program that displays your name, age, and address on three different lines.
+'''
+Name=input("Enter your name:")
+Age=int(input("Enter your age:"))
+Address=input("Enter your address")
+print("Name:",Name,'\n',"Age:",Age,'\n',"Address:",Address)
---
+++ ganga v/day5.1.py
@@ -0,0 +1,10 @@
+'''
+name:ganga
+date:13-11-2024
+version1.1
+Python program that will accept the base and height of a triangle and compute its area.
+'''
+Base=int(input("Enter base:"))
+Height=int(input("Enter height:"))
+Area=(1/2)*Base*Height
+print(f"Area of the triangle with base {Base} and height {Height} is {Area}")
