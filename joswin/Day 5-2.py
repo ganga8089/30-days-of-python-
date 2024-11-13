3b4e0cbf33f3bf8a90ef8e497e9d458957b396be
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-13 23:43:21 +0530

    Add files via upload

---
+++ joswin/Day 5-2.py
@@ -0,0 +1,8 @@
+'''author:Joswin Shaju
+date:13/11/24
+name age & adress on different lines'''
+
+name=input("enter the name:")
+age=int(input("enter the age:"))
+address=input("enter the adress:")
+print("NAME:",name,"\n","AGE:",age,"\n","ADDRESS:",address)
