2360a7d318bd85cb0bb29d3e0da73d52b4df8427
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-13 23:10:51 +0530

    Add files via upload

---
+++ day3.1.py
@@ -0,0 +1,9 @@
+'''
+krishna
+python program to calculate the power of a number using the
+** operator
+'''
+number=int(input("Enter a number: "))
+power=int(input("Enter a power to be raised: "))
+exponentiation=number*power
+print(f"exponentiation of the given number {number} is {exponentiation}")
