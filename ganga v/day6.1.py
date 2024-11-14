08829aa2ba33221256f34a15bee7e8f1db6b5f87
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-14 22:13:13 +0530

    Add files via upload

---
+++ ganga v/day6.1.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:14-11-2024
+version1.1
+Python program to calculate the length of a string.
+'''
+string=input("Enter a string")
+length=len(string)
+print(f"The length of the given string {string} is {length}")
---
+++ ganga v/day6.2.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:14-11-2024
+version1.1
+In the given list print the last and second last element using slicing.
+list=['Red','orange','yellow','black','brown']
+'''
+list=['Red','orange','yellow','black','brown']
+print(list[-2:])
