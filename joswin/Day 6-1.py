0970376789b82a212190ec1860a2d4876d867e0c
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-14 22:36:22 +0530

    Add files via upload

---
+++ joswin/Day 6-1.py
@@ -0,0 +1,7 @@
+'''author:Joswin Shaju
+date:14/11/24
+length of a string'''
+
+str=input("enter the string:")
+length=len(str)
+print("the lenth of the string is:",length)
---
+++ joswin/Day 6-2.py
@@ -0,0 +1,7 @@
+'''author:Joswin Shaju
+date:14/11/24
+last and seccond last elements using slicing'''
+
+list=['Red','orange','yellow','black','brown']
+last_elements=list[-2:]
+print("the second last elements are:",last_elements)
