6ea4acf76534738899b8828f244bf93b7bd56d69
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-15 21:08:23 +0530

    Add files via upload

---
+++ joswin/Day 7-1.py
@@ -0,0 +1,9 @@
+'''author:Joswin Shaju
+date:15/11/24
+check if a number is greater than 5'''
+
+num=int(input("enter the number:"))
+if num>5:
+    print(f"{num} is greater than 5")
+else:
+    print(f"{num} is less than 5")
---
+++ joswin/Day 7-2.py
@@ -0,0 +1,9 @@
+'''author:Joswin Shaju
+date:15/11/24
+check if a number is multiple of 5'''
+
+num=int(input("enter the number:"))
+if num%5==0:
+    print(f"{num} is a multiple of 5")
+else:
+    print(f"{num} is not a multiple of 5")
