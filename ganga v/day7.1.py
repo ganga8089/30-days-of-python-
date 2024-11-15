b6520fae1592b9e965d8a49715604440ada85638
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-15 22:40:09 +0530

    Add files via upload

---
+++ ganga v/day7.1.py
@@ -0,0 +1,9 @@
+'''
+name:Ganga
+date:15-11-2024
+version1.1
+Python program to check whether a number is greater than 5.
+'''
+number=int(input("Enter a number:"))
+if number>5:
+    print(f"The given number {number} is greater than 5!!")
---
+++ ganga v/day7.2.py
@@ -0,0 +1,9 @@
+'''
+name:Ganga
+date:15-11-2024
+version1.1
+Python program that checks if a number is a multiple of 5. If it is, print "Multiple of 5".
+'''
+number=int(input("Enter a number:"))
+if number % 5 == 0:
+    print("Multiples of 5!!")
