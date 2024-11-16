96875334cc3e19d8afd0ebdee89231c9aec1e4eb
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-17 00:40:23 +0530

    Delete joswin/Day 8-1.py

--- joswin/Day 8-1.py
+++
@@ -1,9 +0,0 @@
-'''author:Joswin Shaju
-date:16/11/24
-to check the eligibility to vote'''
-
-age=int(input("enter you age:"))
-if age>18:
-    print("the voter is eligible to vote")
-else:
-    print("the voter is not eligible to vote")
