042047ab0d2e7b05630ad9aaea71861a56d93e54
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-17 00:23:45 +0530

    Delete day3.1.py

--- day3.1.py
+++
@@ -1,9 +0,0 @@
-'''
-krishna
-python program to calculate the power of a number using the
-** operator
-'''
-number=int(input("Enter a number: "))
-power=int(input("Enter a power to be raised: "))
-exponentiation=number*power
-print(f"exponentiation of the given number {number} is {exponentiation}")
