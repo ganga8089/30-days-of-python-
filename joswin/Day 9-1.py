32e1ad2a56be3ec3329a08a923b423c9187c4945
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-17 23:16:37 +0530

    Add files via upload

---
+++ joswin/Day 9-1.py
@@ -0,0 +1,17 @@
+'''author:Joswin Shaju
+date:17/11/24
+to assign grades'''
+
+std_grades=int(input("enter the grade:"))
+if std_grades>=90:
+    print("Grade:A")
+elif std_grades>=80:
+    print("Grade:B")
+elif std_grades>=70:
+    print("Grade:C")
+elif std_grades>=60:
+    print("Grade:D")
+elif std_grades>=50:
+    print("Grade:E")
+else:
+    print("Grade:F")
---
+++ joswin/Day 9-2.py
@@ -0,0 +1,13 @@
+'''author:Joswin Shaju
+date:17/11/24
+ckeck numbers within a specified range'''
+
+num=int(input("enter the number:"))
+if num>=10 and num<=20:
+    print("the number is between 10 and 20")
+elif num>=20 and num<=30:
+    print("the number is between 20 and 30")
+elif num>=30 and num<=40:
+    print("the number is between 30 and 40 ")
+else :
+    print("out of range")
