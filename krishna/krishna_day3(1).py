3fc977538a4f2e85596644163d3f58877ee692f5
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-18 22:37:40 +0530

    Update and rename krishna_day3.py to krishna_day3(1).py

--- krishna/krishna_day3(1).py
+++ krishna/krishna_day3(1).py
@@ -1,6 +1,6 @@
 '''
 krishna
-python program t
+python program to calculate the power of a number using the ** operator.
 '''
 num1=int(input("Enter a number:"))
 exponent=int(input("Enter the exponent:"))
