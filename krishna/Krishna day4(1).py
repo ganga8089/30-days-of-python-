e8ea0c0916d141bfe8e0e29d2970d31b15c6036c
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-18 22:45:06 +0530

    Create Krishna day4(1).py

---
+++ krishna/Krishna day4(1).py
@@ -0,0 +1,7 @@
+'''
+krishna
+Write a python program that accepts the user's first and last name and prints them in reverse 
+'''
+first_name=input("Enter your first name")
+last_name=input("Enter your last name")
+print(f"{last_name} {first_name}")
