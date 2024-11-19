327e76c1feea8ad9abfac25fac04a0510c02e0e6
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-19 22:50:13 +0530

    Add files via upload

---
+++ joswin/Day 11-1.py
@@ -0,0 +1,6 @@
+'''author:Joswin Shaju
+date:19/11/24
+print first 10 natural numbers '''
+
+for i in range(1,11):
+    print(i)
---
+++ joswin/Day 11-2.py
@@ -0,0 +1,10 @@
+'''author:Joswin Shaju
+date:19/11/24
+print even numbers upto 50 and their sum '''
+
+
+a=0
+for i in range(2,51,2):
+    a+=i
+    print(i)
+print("sum:",a)
