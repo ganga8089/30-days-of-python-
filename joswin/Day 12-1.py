4a45af760b93ced94d0dccb5f244856c29cca545
JOSWIN SHAJU <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-11-21 00:09:36 +0530

    Add files via upload

---
+++ joswin/Day 12-1.py
@@ -0,0 +1,11 @@
+'''author:Joswin Shaju
+date:20/11/24
+print even numbers upto 50 and their sum '''
+
+
+limit=int(input("enter the range:"))
+new_list=[]
+for i in range(limit):
+    ele=int(input("enter the elements:"))
+    new_list.append(ele)
+print("the new list is:",new_list)
