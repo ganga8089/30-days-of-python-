ddc3bd88f5962bb71a7baaf1abfbc3f74298fa9b
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-11-21 22:39:58 +0530

    Add files via upload

---
+++ joseph/day 9 second.py
@@ -0,0 +1,7 @@
+number=int(input("Enter the number:"))
+if 30<=number<=40:
+    print("The number is between 30 and 40")
+elif 10<=number<=20:
+    print("The number is between 10 and 20")
+else:
+    print("Out of range")
