d6a5b61a0034f8774a671c9906d01a431febb0e9
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-27 22:06:22 +0530

    Add files via upload

---
+++ ganga v/day_18.1.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:27-11-2024
+version1.1
+Python program to find the maximum and minimum values in a tuple.
+'''
+my_tuple=(1,5,2,8,9)
+print("Maximum value=",max(my_tuple))
+print("Minimum value=",min(my_tuple))
---
+++ ganga v/day_18.2.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:27-11-2024
+version1.1
+Python program to reverse a tuple.
+'''
+my_tuple=(1,3,5,7,9)
+reversed_tuple=my_tuple[::-1]
+print("Reversed tuple=",reversed_tuple)
