1f9a7c1fc2751684fd4ab74c5cd2750951e49f96
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-27 21:47:40 +0530

    Add files via upload

---
+++ ganga v/day_17.1.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:26-11-2024
+version1.1
+Python program to create a tuple and print its elements.
+'''
+fruits=("apple","mango","cherry","dates","banana")
+for i in fruits:
+    print(i)
---
+++ ganga v/day_17.2.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+date:26-11-2024
+version1.1
+Python program to find the length of a tuple.
+'''
+my_tuple=(1,2,3,4,5)
+length=len(my_tuple)
+print(length)
