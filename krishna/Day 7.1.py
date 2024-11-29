ab9a413402f97e2e13df943dceb0658e49eb59b9
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-30 00:38:19 +0530

    Create Day 7.1.py

---
+++ krishna/Day 7.1.py
@@ -0,0 +1,8 @@
+'''
+krishna
+python program to check whether a number is greater than 5.
+'''
+
+number=int(input("Enter a number:"))
+if number>5:
+    print(f"The given number {number} is greater than 5")
