1ceb6fc3754560460e0202421e2c8f6d92e12bfe
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-29 23:55:22 +0530

    Create Day 6.2.py

---
+++ krishna/Day 6.2.py
@@ -0,0 +1,7 @@
+'''
+krishna
+python program to print the last and second last element using slicing. list=['Red','orange','yellow',black',brown']
+'''
+
+list=['Red','orange','yellow','black','brown']
+print(list[-2:])
