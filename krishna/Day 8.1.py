53d25aca72bb20eae013d610280f3b3b32bee2ac
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-30 00:51:00 +0530

    Create Day 8.1.py

---
+++ krishna/Day 8.1.py
@@ -0,0 +1,10 @@
+'''
+krishna
+python program to check whether a person is eligible for vote or not.
+'''
+
+age=int(input("enter you age:"))
+if age>=18:
+    print("the voter is eligible to vote")
+else:
+    print("the voter is not eligible to vote")
