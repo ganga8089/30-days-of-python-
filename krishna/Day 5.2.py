4acd335a42689185ed74bda601cac33389ba6926
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-29 23:45:26 +0530

    Create Day 5.2.py

---
+++ krishna/Day 5.2.py
@@ -0,0 +1,9 @@
+'''
+krishna
+python program that displays your name, age, and address on three different lines.
+'''
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+address=input("Enter your address")
+print("name:",name,'\n',"age:",age,'\n',"address:",address)
