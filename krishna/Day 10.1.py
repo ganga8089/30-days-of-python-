cfcf85ba3e229b1e9555057bf7a130b7e39e1410
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-30 01:37:44 +0530

    Create Day 10.1.py

---
+++ krishna/Day 10.1.py
@@ -0,0 +1,11 @@
+'''
+krishna
+python program to accept two numbers and perform the arithmetic 
+'''
+
+str=input("enter the string:")
+palindrome=str[::-1]
+if str==palindrome:
+    print(f"{str} is a palindrome")
+else:
+    print(f"{str} is not a palindrome")
