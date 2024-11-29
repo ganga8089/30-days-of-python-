e5e2cc0e5638b29b2030d987b3178c88edc07916
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-11-29 22:59:35 +0530

    Add files via upload

---
+++ ganga v/day_19.1.py
@@ -0,0 +1,10 @@
+'''
+name:ganga
+date:29-11-2024
+version1.1
+Create two sets set_a = {1, 2, 3} and set_b = {2, 3, 4}. Find and print the intersection of these two sets.
+'''
+set_a={1,2,3}
+set_b={2,3,4}
+intersection_sets=set_a & set_b
+print("Intersection of set_a and set_b is ",intersection_sets)
---
+++ ganga v/day_19.2.py
@@ -0,0 +1,10 @@
+'''
+name:ganga
+date:29-11-2024
+version1.1
+Create two sets set_a = {1, 2, 3} and set_b = {3, 4, 5}. Find and print the union of these two sets.
+'''
+set_a={1,2,3}
+set_b={3,4,5}
+union_sets=set_a | set_b
+print("Union of set_a and set_b is ",union_sets)
