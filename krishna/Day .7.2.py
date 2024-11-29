35fd3e63aa6d832e296255fa8a8a18c223a13a86
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-11-30 00:40:52 +0530

    Create Day 7.2.py

---
+++ krishna/Day .7.2.py
@@ -0,0 +1,8 @@
+'''
+krishna
+python program Write a program that checks if a number is a multiple of 5. If it is, print "Multiple of 5". 
+'''
+
+number=int(input("Enter a number"))
+if number % 5 == 0:
+    print("Multiples of 5")
